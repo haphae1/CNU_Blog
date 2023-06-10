d33450770b9f3e05d0b5749153bca1acccf89ef4
haphae1 <njs8085@naver.com>
2023-06-10 18:21:13 +0900

    prac

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Layout from './pages/Layout';
-import Post from './pages/Post';
 import Resume from './pages/Resume';
-import Write from './pages/Write';
 import Header from './components/Header';
+import Post from './pages/Post.tsx';
+import Write from './pages/Write.tsx';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.DEV ? '/' : '/CNU_Blog/'}>
       <Routes>
         <Route element={<Header />}>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/resume" element={<Resume />} />
           </Route>
-          {/*todo (3-3) Post 추가*/}
+          <Route path="/post/:postId" element={<Post />} />
         </Route>
-        {/*todo (5-1) Write 추가*/}
+        <Route path="/write" element={<Write />} />
       </Routes>
     </Router>
   );
--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,30 @@
-import axios from 'axios';
-import { IPostWithAd, IResponsePostList } from './types';
+// eslint-disable-next-line import/named
+import axios, { AxiosResponse } from 'axios';
+import { IResponsePostList } from './types';
 
 const instance = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL: '',
+  baseURL: 'http://localhost:8000',
 });
 
 // todo (6) api 작성
 
-export const getPostList = () => {
-  return null;
+export const getPostList = (): Promise<AxiosResponse<IResponsePostList>> => {
+  return instance.get('/posts');
 };
 
-export const createPost = () => {
-  return null;
+export const createPost = (title: string, contents: string, tag: string) => {
+  return instance.post('/posts', {
+    title,
+    contents,
+    tag,
+  });
 };
 
-export const getPostById = () => {
-  return null;
+export const getPostById = (id: number) => {
+  return instance.get('post/${id}');
 };
 
 export const updatePostById = () => {
